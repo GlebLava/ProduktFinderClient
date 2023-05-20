9ff1569a0f224046840b13a09ecd98133c7b08fe
GlebLava <60477842+GlebLava@users.noreply.github.com>
2023-05-20 21:01:28 +0200

    Kinda done now, but this commit is also for testing on laptop and pc at same time. So mainly transfer

--- ProduktFinderClient/App.xaml.cs
+++ ProduktFinderClient/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using ProduktFinderClient.DataTypes;
+using ProduktFinderClient.Models;
 using ProduktFinderClient.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,27 +11,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ProduktFinderClient
+namespace ProduktFinderClient;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    public static event EventHandler? MainWindowCloseEvent;
 
-    public partial class App : Application
+    public static MainWindow mainWindow = new();
+
+    protected override void OnStartup(StartupEventArgs e)
     {
-        public static MainWindow mainWindow = new();
+        OptionsWindowViewModel optionsWindowViewModel = new OptionsWindowViewModel();
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            OptionsWindowViewModel optionsWindowViewModel = new OptionsWindowViewModel();
+        MainWindowViewModel mainWindowViewModel = new MainWindowViewModel(optionsWindowViewModel, mainWindow.MainPartsGrid, mainWindow.MainStatusBlock);
+        mainWindow.DataContext = mainWindowViewModel;
 
-            MainWindowViewModel mainWindowViewModel = new MainWindowViewModel(optionsWindowViewModel, mainWindow.MainPartsGrid, mainWindow.MainStatusBlock);
-            mainWindow.DataContext = mainWindowViewModel;
-
-
-            mainWindow.Show();
-            base.OnStartup(e);
-        }
-
+        mainWindow.Show();
+        base.OnStartup(e);
     }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        MainWindowCloseEvent?.Invoke(null, EventArgs.Empty);
+        // For some reason when I tried to have Unregister subscribed to MainWindowCloseEvent
+        // it would not be waited upon. Because this is a very important call, I explicitly put it here
+        // If a somebody knows why, they can put it back into RequestHandler and have it to be called through a
+        // subscription
+        RequestHandler.Unregister().Wait();
+        base.OnExit(e);
+    }
+
 }
